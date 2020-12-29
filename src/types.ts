8b0af093385721dc53b9897f63a8080d18f8746c
Chris Thomas <tchrs@uber.com>
2020-12-28 21:24:50 -0500

    Added eslint (#3)

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,21 @@
-
 export type Member = {
-  name: string,
-  days: MemberDay[],
-  score: number,
-  lastAttempted: number
-}
+  name: string;
+  days: MemberDay[];
+  score: number;
+  lastAttempted: number;
+};
 
 export type MemberDay = {
-  day: number,
-  star1?: Star,
-  star2?: Star,
-  score: number
-}
+  day: number;
+  star1?: Star;
+  star2?: Star;
+  score: number;
+};
 
 export type Star = {
-  index: number,
-  timestamp: number,
-  duration: number,
-  gaveUp?: boolean,
-  position?: number,
-}
+  index: number;
+  timestamp: number;
+  duration: number;
+  gaveUp?: boolean;
+  position?: number;
+};
